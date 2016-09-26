b02dc0a8ca8e55e98065088bbaf258ab9de380a8
samx1118 <samx_48783_@hotmail.com>
2016-09-26 18:43:14 +0700

    LastQHW2

--- a2/DateUtil.scala
+++ a2/DateUtil.scala
@@ -1,12 +1,14 @@
 object DateUtil extends App {
   type Date = (Int, Int, Int)
+  def normalMonths: List[Int] = List(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+  def leapMonths:   List[Int] = List(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
   def isOlder(x: Date, y: Date): Boolean = {
     if (x._3 < y._3) true else
     if (x._3 > y._3) false else {
       if (x._2 < y._2) true else
       if (x._2 > y._2) false else {
-        if (x._1 < y._2) true else false
+        if (x._1 < y._1) true else false
       }
     } 
   }
@@ -39,13 +41,20 @@ object DateUtil extends App {
     }
   }
 
+  def getMonth(month: Int, months: List[String]): String = if (month == 1) months.head else getMonth(month - 1, months.tail)
+
   def dateToString(d: Date): String = {
-    def getMonth(month: Int, months: List[String]): String = if (month == 1) months.head else getMonth(month - 1, months.tail)
     var month: String = getMonth(d._2, List("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
     month + "-" + d._1 + "-" + d._3
   }
+  def isLeapYear(yr: Int): Boolean = (yr % 4 == 0)
 
-  def whatMonth(n: Int, yr: Int): Int = ???
+  def whatMonth(n: Int, yr: Int): Int = {
+    def calMonth(d: Int, m: Int, ds: List[Int]): Int = {
+      if (d <= 0) m else calMonth(d - ds.head, m + 1, ds.tail)
+    }
+    if (isLeapYear(yr)) calMonth(n, 0, leapMonths) else calMonth(n, 0, normalMonths)
+  }
 
   def oldest(dates: List[Date]): Option[Date] = {
     def oldestH(dates: List[Date], oldDate: Date): Option[Date] = {
@@ -56,5 +65,10 @@ object DateUtil extends App {
     if (dates.isEmpty) None else oldestH(dates.tail, dates.head)
   }
 
-  def isReasonableDate(d: Date): Boolean = ???
+  def isReasonableDate(d: Date): Boolean = {
+    def getMonthMax(m: Int, months: List[Int]): Int = if (m == 1) months.head else getMonthMax(m - 1, months.tail)
+    var monthMax = getMonthMax(d._2, normalMonths)
+    if (d._3 == 0) false else
+    if (isLeapYear(d._3) && d._2 == 2) d._1 <= (monthMax + 1) else d._1 <= monthMax
+  }
 }
--- a2/Zombies.scala
+++ a2/Zombies.scala
@@ -1,3 +1,32 @@
 object Zombies extends App {
-  def countBad(hs: List[Int]): Int = ???
+  def countBad(hs: List[Int]): Int = {
+  	def mergeList(hs: List[Int], ts: List[Int]): List[Int] = {
+  		if (hs.isEmpty) ts else hs.head :: mergeList(hs.tail, ts) 
+  	}
+
+	def splitList(hs: List[Int], n: Int): (List[Int], List[Int]) = {
+		if (n == 0) (Nil, hs) else {
+			var spltLst: (List[Int], List[Int]) = splitList(hs.tail, n - 1)
+			(hs.head :: spltLst._1, spltLst._2)
+		}
+	}
+
+	def countBadH(hs: List[Int], ts: List[Int], count:Int): (List[Int], Int) = {
+		if (hs.isEmpty || ts.isEmpty) (mergeList(hs, ts), count) else {
+			if (hs.head <= ts.head) {
+				var lstCount: (List[Int], Int) = countBadH(hs.tail, ts, count)
+				(hs.head :: lstCount._1, lstCount._2)
+			} else { 
+				var lstCount: (List[Int], Int) = countBadH(hs, ts.tail, count + hs.length)
+				(ts.head :: lstCount._1, lstCount._2)
+			}
+		}
+	}
+
+	if (hs.length == 1) 0 else {
+		var headTail: (List[Int], List[Int]) = splitList(hs, hs.length/2)
+		var countHead: Int = countBad(headTail._1)
+
+	}
+  }
 }
